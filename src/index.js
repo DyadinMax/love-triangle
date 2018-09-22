8311dd28071f35273a9ee4b3b0919c64985adbfb
DyadinMax <43500937+DyadinMax@users.noreply.github.com>
2018-09-22 23:08:31 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,15 +4,16 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
-  var len = preferences.length, n = 0;
+  let len = preferences.length;
+  var n = 0;
   for (let i = 0; i < len; i++)
   {
     let a = preferences[i] - 1;
     if (a == i) continue;
     let b = preferences[a] - 1;
-    if (b = a) continue;
+    if (b == a) continue;
     let c = preferences[b] - 1;
-    if (c = b) continue;
+    if (c == b) continue;
     if (c == i)
     {
       n++;
